4d5461c1c2d44212fb5f020e4c3b0db932bc3bfe
Kevin.OMalley <Kevin.OMalley@ul.ie>
2023-04-20 15:41:49 +0100

    added docs and some functions

---
+++ fin2py/docs/about.md
@@ -0,0 +1,17 @@
+# About this package
+
+Fino2py is a Python package designed to automate the ingestion and reshaping of data produced by the Finapres Finometer, a device used to measure continuous blood pressure and related cardiovascular variables. The package provides functions to read in raw Finometer data from .txt files, extract chunks of data based on experimental protocols for each participant, and concatenate participants together to allow for group-level analysis.
+
+The aim of Fino2py is to streamline the data analysis process for researchers and clinicians working with Finometer data, enabling them to focus on their research questions rather than the technical details of data processing. By automating the data processing tasks, Fino2py helps to minimize the potential for human error, reduce processing time, and increase the reproducibility of results.
+
+This package was created by Kevin O'Malley to assist research in the Department of Psychology at the University of Limerick. Feedback and collaberation is most welcome and can be sent to kevin.omalley[@]ul.ie.
+
+If you use this package in your work, please reference this work: 
+
+<<<Insert reference>>>
+
+
+
+
+
+
---
+++ fin2py/src/import_protocol_times.py
@@ -0,0 +1,85 @@
+
+
+def import_protocol_times(times_file_path: pl.Path, add_seconds: bool = False, flatten_seconds: bool = False, save_csv: bool = False) -> pd.DataFrame:
+    '''
+    This function imports the protocol times from a .csv file and returns a cleaned pandas dataframe with the protocol times for each participant.
+
+    Parameters
+    ----------
+    times_file_path : pathlib.Path
+        The path to the .csv file containing the protocol times.
+    add_seconds : bool, optional
+        If True, seconds will be added to the time values (if missing).
+    flatten_seconds : bool, optional
+        If True, seconds will be set to 00 for all time values.
+    save_csv : bool, optional
+        If True, the imported data will be saved as a .csv file in the same folder as the .csv file.
+
+    Raises
+    ------
+    TypeError:
+        If times_file_path is not a pathlib.Path object.
+    ValueError:
+        If times_file_path does not exist or is not a file.
+        If times_file_path does not have a .csv extension.
+        If add_seconds and flatten_seconds are both True.
+        If the function is unable to add seconds to time values or set seconds to 00.
+
+    Returns
+    -------
+    pandas.DataFrame
+        A cleaned pandas dataframe with the protocol times for each participant.
+    '''
+
+    def add_seconds_to_time(time_str):
+        '''This function adds seconds to the time string for in case the time string is missing seconds'''
+        if len(time_str) == 5:
+            time_str += ":00"
+        return time_str
+
+    def flatten_seconds(time_str):
+        '''This function sets seconds to 00 for a given time string'''
+        return time_str[:5] + ':00'
+
+    if not isinstance(times_file_path, pl.Path):#check if folder_path is a pathlib.Path object
+        raise TypeError('file_path must be a pathlib.Path object')
+    elif not times_file_path.exists(): #  and if it exists
+        raise ValueError('file_path does not exist')
+    elif not times_file_path.is_file(): #  and is a file 
+        raise ValueError('file_path is not a file')
+    elif times_file_path.suffix != '.csv': #  and is a csv file
+        raise ValueError('file_path is not an csv file')
+    else:
+        df = pd.read_csv(times_file_path, delimiter= ',')
+        df.columns = [col.strip() for col in df.columns]
+        cols_to_keep = ['Participant ID', 'Start of Baseline', 'End of Baseline', 'Start of Task 1', 'End of Task 1', 'Start of Recovery Period', 'End of Recovery Period']
+        df = df[cols_to_keep].applymap(lambda x: str(x).strip('"') if isinstance(x, str) else x)
+        
+        if add_seconds and flatten_seconds:
+            raise ValueError('Only one of add_seconds and flatten_seconds can be True')
+
+        if add_seconds:
+            try:
+                df.iloc[:, 1:] = df.iloc[:, 1:].applymap(lambda x: add_seconds_to_time(x) if isinstance(x, str) else x)
+                df.iloc[:, 1:] = df.iloc[:, 1:].applymap(lambda x: pd.to_datetime(x, format='%H:%M:%S', errors='coerce'))
+            except:
+                print('Could not add seconds to time, please check the time format')
+
+        elif flatten_seconds:
+            try:
+                df.iloc[:, 1:] = df.iloc[:, 1:].applymap(lambda x: add_seconds_to_time(x) if isinstance(x, str) else x)
+                df.iloc[:, 1:] = df.iloc[:, 1:].applymap(lambda x: flatten_seconds(x) if isinstance(x, str) else x)
+                df.iloc[:, 1:] = df.iloc[:, 1:].applymap(lambda x: pd.to_datetime(x, format='%H:%M:%S', errors='coerce'))
+            except:
+                print('Could not set seconds to 00, please check the time format')
+        
+        if save_csv: #if you want to save the csv file (which may be useful if you want to use the data in other ways)
+            try:
+                df.to_csv(times_file_path.parent / f"cleaned times.csv", index=False)
+                print(f"CSV saved for {times_file_path.stem}")
+            except Exception as e:
+                print(f"Could not save csv file, error: {e}")
+            
+
+
+        return df

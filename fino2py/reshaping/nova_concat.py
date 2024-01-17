8f3fa379fb341f2977e21a3071af42fe54fe6a2c
Kevin.OMalley <Kevin.OMalley@ul.ie>
2024-01-17 12:44:42 +0000

    again trying to update docs
    The previous commit wasn't allowing the docs to update properly

--- fino2py/reshaping/nova_concat.py
+++ fino2py/reshaping/nova_concat.py
@@ -5,6 +5,37 @@
 from ..dependencies import pd, Union, Dict 
 from .nova_minute_by_minute import nova_minute_by_minute
 
+'''
+This function allows us to concatenate the minute-by-minute data for each participant into a single DataFrame so that we can perform group-level analysis.
+
+In future when we have a means of creating protocol 'chunk' averages, we will also be able to use this function to concatenate the chunk averages for each participant into a single DataFrame for group-level analysis. 
+But for now this is just for minute-by-minute data.
+
+Notes
+-----
+- The `data_dict` parameter should be a dictionary with participant IDs as keys and minute-by-minute dataframes as values.
+- The `data_dict` should be created using the `read_raw_nova_data` function.
+- The Participant ID column is added at the beginning of the DataFrame to allow for sample-level analysis, and to allow for easy merging of various datasets (i.e. demographics data and or survey data from qualtrics or pavlovia).
+
+example
+-------
+# Import minute-by-minute data for each participant
+
+data_dict: Dict[str, pd.DataFrame] = {}
+
+for sub_folder in project_folder.iterdir():
+    try:
+        ID, df = read_raw_nova_data(sub_folder, interval='1s', save_csv=True)
+
+        data_dict[ID] = df
+
+    except Exception as e:
+        print(f'Error processing folder {sub_folder.name}: {e}')
+}
+
+concatenated_df = nova_concat(data_dict)
+'''
+
 def nova_concat(data_dict: Dict) -> pd.DataFrame:
     '''
     Concatenate the minute-by-minute data for each participant into a single DataFrame.
